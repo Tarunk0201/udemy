e1a999f5c6fadef3b06c1bd9e58e1fe2a3c49b1e
Tarunk0201 <tarun.singh.7801@gmail.com>
2025-04-16 11:20:28 +0530

    jQuery

--- jquery/index.js
+++ jquery/index.js
@@ -1,1 +1,13 @@
-$("h1").css("color", "red");
+$("h1").before("<button>new</button>")
+
+
+$("button").click(function(){
+    $("h1").css("color", "purple");
+})
+
+$("input").keypress(function(event){
+    $("h1").text(event.key );
+});
+$("h1").on("mouseover", function(){
+    $("h1").css("color", "brown");
+})
